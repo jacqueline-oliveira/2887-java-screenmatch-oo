732776f6af08aa993d80cbe5367cddd09a9dd5cb
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-03-15 08:34:44 -0300

    video 1.3

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        System.out.println(meuFilme.nome);
+        System.out.println(meuFilme.anoDeLancamento);
+
+    }
+}
