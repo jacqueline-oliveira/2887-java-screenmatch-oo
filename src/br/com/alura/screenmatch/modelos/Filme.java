5ada6c1864c4f0f22ca36fc1d0cc20a699a0fa8d
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-03-15 10:00:22 -0300

    video 4.2

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -15,6 +15,6 @@ public class Filme extends Titulo implements Classificavel {
 
     @Override
     public int getClassificacao() {
-        return 0;
+        return (int) pegaMedia() / 2;
     }
 }
