fea9fdfa4597a3007892b3d2d50fa0723186f8fd
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-03-15 08:38:09 -0300

    video 1.4

--- src/Filme.java
+++ src/Filme.java
@@ -5,4 +5,18 @@ public class Filme {
     double somaDasAvaliacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -5,8 +5,13 @@ public class Principal {
         meuFilme.anoDeLancamento = 1970;
         meuFilme.duracaoEmMinutos = 180;
 
-        System.out.println(meuFilme.nome);
-        System.out.println(meuFilme.anoDeLancamento);
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
 
     }
 }
