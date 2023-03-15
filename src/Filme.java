c369f8f1aca5cc460b6556e3900c24ae6d5460cd
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-03-15 08:31:03 -0300

    video 1.2

---
+++ src/Filme.java
@@ -0,0 +1,8 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+}
